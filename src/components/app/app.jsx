4854875d96dc88b7fc7755112c3c794b15f4a10d
vasylKarpovich <karpovichvasya@gmail.com>
2024-08-04 17:22:52 +0300

    improved the code. Added data to file app.js

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,25 +1,31 @@
-import AppInfo from '../app-info/app-info';
-import SearchPanel from '../search-panel/search-panel';
-import AppFilter from '../app-filter/app-filter';
-import EmployeesList from '../employees-list/employees-list';
-import EmployeesAddForm from '../employees-add-form/employees-add-form';
+import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployeesList from "../employees-list/employees-list";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
-import './app.css';
+import "./app.css";
 
 function App() {
-   return (
-      <div className="app">
-         <AppInfo />
+  const data = [
+    { name: "Василь Карпович", salary: 800, increase: false },
+    { name: "Ксенія Гришко", salary: 3000, increase: true },
+    { name: "Ігор Усенко", salary: 5000, increase: false },
+  ];
 
-         <div className="search-panel">
-            <SearchPanel />
-            <AppFilter />
-         </div>
+  return (
+    <div className="app">
+      <AppInfo />
 
-         <EmployeesList />
-         <EmployeesAddForm />
+      <div className="search-panel">
+        <SearchPanel />
+        <AppFilter />
       </div>
-   );
+
+      <EmployeesList data={data} />
+      <EmployeesAddForm />
+    </div>
+  );
 }
 
 export default App;
--- src/components/employees-list/employees-list.jsx
+++ src/components/employees-list/employees-list.jsx
@@ -1,15 +1,14 @@
-import EmployeesListItem from '../employees-list-item/employees-list-item';
+import EmployeesListItem from "../employees-list-item/employees-list-item";
 
-import './employees-list.css';
+import "./employees-list.css";
 
-const EmployeesList = () => {
-   return (
-      <un className="app-list list-group">
-         <EmployeesListItem />
-         <EmployeesListItem />
-         <EmployeesListItem />
-      </un>
-   );
+const EmployeesList = ({ data }) => {
+  const elements = data.map((item) => {
+    return <EmployeesListItem {...item} />;
+    // name={item.name} salary={item.salary} ---- таке буде, якщо використати спред оператор, як ми і використовуємо, написавши: ...item
+  });
+
+  return <ul className="app-list list-group">{elements}</ul>;
 };
 
 export default EmployeesList;
