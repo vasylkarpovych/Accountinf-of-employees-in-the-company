01913bdbc0e223d55bddbda243beab6c1530889d
Vasyl <karpovichvasya@gmail.com>
2024-09-02 16:39:36 +0300

    created the onToggleProp method and optimized app according to the DRY pronciple

--- src/components/app-info/app-info.jsx
+++ src/components/app-info/app-info.jsx
@@ -1,14 +1,11 @@
 import './app-info.css';
 
-const AppInfo = ({ data }) => {
+const AppInfo = ({ employees, increased }) => {
    return (
       <div className="app-info">
          <h1>Облік співробітникув в компанії "Рога і Копита"</h1>
-         <h2>Загальна кількість співробтників: {data.length}</h2>
-         <h2>
-            Премію отримують:{' '}
-            {data.filter((riseUser) => riseUser.rise === true).length}
-         </h2>
+         <h2>Загальна кількість співробтників: {employees}</h2>
+         <h2>Премію отримують: {increased}</h2>
       </div>
    );
 };
--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -73,28 +73,56 @@ class App extends Component {
       });
    };
 
-   onToggleIncrease = (id) => {
-      // this.setState(({ data }) => {
-      // const index = data.findIndex((elem) => elem.id === id);
+   // onToggleIncrease = (id) => {
+   //    // this.setState(({ data }) => {
+   //    // const index = data.findIndex((elem) => elem.id === id);
 
-      // const old = data[index];
-      // const newItem = { ...old, increase: !old.increase };
-      // const newArr = [
-      //    ...data.slice(0, index),
-      //    newItem,
-      //    ...data.slice(index + 1),
-      // ];
+   //    // const old = data[index];
+   //    // const newItem = { ...old, increase: !old.increase };
+   //    // const newArr = [
+   //    //    ...data.slice(0, index),
+   //    //    newItem,
+   //    //    ...data.slice(index + 1),
+   //    // ];
 
-      // return {
-      //    data: newArr,
-      // };
-      // });
+   //    // return {
+   //    //    data: newArr,
+   //    // };
+   //    // });
+   //    this.setState(({ data }) => ({
+   //       data: data.map((item) => {
+   //          if (item.id === id) {
+   //             return {
+   //                ...item,
+   //                increase: !item.increase,
+   //             };
+   //          }
+   //          return item;
+   //       }),
+   //    }));
+   // };
+
+   // onToggleRise = (id) => {
+   //    this.setState(({ data }) => ({
+   //       data: data.map((item) => {
+   //          if (item.id === id) {
+   //             return {
+   //                ...item,
+   //                rise: !item.rise,
+   //             };
+   //          }
+   //          return item;
+   //       }),
+   //    }));
+   // };
+
+   onToggleProp = (id, prop) => {
       this.setState(({ data }) => ({
          data: data.map((item) => {
             if (item.id === id) {
                return {
                   ...item,
-                  increase: !item.increase,
+                  [prop]: !item[prop],
                };
             }
             return item;
@@ -102,14 +130,13 @@ class App extends Component {
       }));
    };
 
-   onToggleRise = (id) => {
-      console.log(`Rise this ${id}`);
-   };
-
    render() {
+      const employees = this.state.data.length;
+      const increased = this.state.data.filter((item) => item.increase).length;
+
       return (
          <div className="app">
-            <AppInfo data={this.state.data} />
+            <AppInfo employees={employees} increased={increased} />
 
             <div className="search-panel">
                <SearchPanel />
@@ -119,8 +146,7 @@ class App extends Component {
             <EmployeesList
                data={this.state.data}
                onDelete={this.deleteItem}
-               onToggleIncrease={this.onToggleIncrease}
-               onToggleRise={this.onToggleRise}
+               onToggleProp={this.onToggleProp}
             />
             <EmployeesAddForm onAdd={this.addItem} />
          </div>
